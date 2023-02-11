dc3ad032b0633050782d63d86912c70e3894f1bb
Miguel Rico Feo <miguelrico2031@gmail.com>
2023-02-11 15:02:43 +0100

    puertas logica de habitaciones colliders mejor puestos
    yea

---
+++ GameJamVS2023_/Assets/Miguel/Scripts/CorridorManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorridorManager : MonoBehaviour
+{
+    private GameObject _door;
+    // Start is called before the first frame update
+    public bool Open = false;
+
+    private void OnEnable()
+    {
+        DungeonManager.Instance.DungeonGenerated.AddListener(OnDungeonGenerated);
+    }
+
+    private void OnDisable()
+    {
+        DungeonManager.Instance.DungeonGenerated.RemoveListener(OnDungeonGenerated);
+    }
+
+    // Update is called once per frame
+    void OnDungeonGenerated()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            if (child.CompareTag("Door"))
+            {
+                _door = child.gameObject;
+                break;
+            }
+        }
+
+    }
+
+    public void OpenDoor()
+    {
+        _door.SetActive(false);
+        Open = true;
+    }
+
+    public void CloseDoor()
+    {
+        _door.SetActive(true);
+        Open = false;
+    }
+}
