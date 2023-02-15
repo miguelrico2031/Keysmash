13361bfcd12411e3874f306657c62c4affffc68b
Borjita <71882875+Vorja83@users.noreply.github.com>
2023-02-15 20:52:08 +0100

    s

--- GameJamVS2023_/Assets/Miguel/Scripts/Boss/Boss.cs
+++ GameJamVS2023_/Assets/Miguel/Scripts/Boss/Boss.cs
@@ -1,6 +1,7 @@
     using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Boss : MonoBehaviour
 {
@@ -158,7 +159,7 @@ public class Boss : MonoBehaviour
 
     public void BossDead()
     {
-        Debug.Log("Muelto");
+        SceneManager.LoadScene("Credits");
     }
 
 }
