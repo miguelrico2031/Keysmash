ffae39fca91224101865a059ed3d94b362748f02
Miguel Rico Feo <miguelrico2031@gmail.com>
2023-02-15 01:23:58 +0100

    lagarto hijueputa el diabloooo

--- GameJamVS2023_/Assets/Miguel/Scripts/Enemies/FireBall.cs
+++ GameJamVS2023_/Assets/Miguel/Scripts/Enemies/FireBall.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class FireBall : MonoBehaviour
 {
-    
+    [HideInInspector] public int Damage;
+    [HideInInspector] public float Speed, RotationSpeed, KnockbackForce, KnockbackDuration;
+
+    private Rigidbody2D _rb;
+
+    private GameObject _player;
+    private StatsManager _playerStats;
+
+    private Vector2 _directionToPlayer;
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _playerStats = _player.GetComponent<StatsManager>();
+
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _directionToPlayer = (_player.transform.position - transform.position).normalized;
+
+        _rb.velocity = _directionToPlayer * Speed * Time.fixedDeltaTime;
+       
+        float angle = Mathf.Atan2(_directionToPlayer.y, _directionToPlayer.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation =  Quaternion.RotateTowards(transform.rotation, rotation, RotationSpeed * Time.fixedDeltaTime);
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log(other.gameObject.name);
+        if(other.CompareTag("Keyboard")) Destroy(gameObject);
+        
+        else if(other.gameObject.CompareTag("Player"))
+        {
+            _playerStats.TakeDamage(new EnemyAttack(Damage, _directionToPlayer * KnockbackForce, KnockbackDuration));
+            Destroy(gameObject);
+        }
+        
+        else if(other.gameObject.layer == LayerMask.NameToLayer("Walls")) Destroy(gameObject);        
+    }
+
 }
--- GameJamVS2023_/Assets/Miguel/Scripts/Enemies/Lizard.cs
+++ GameJamVS2023_/Assets/Miguel/Scripts/Enemies/Lizard.cs
@@ -4,31 +4,105 @@ using UnityEngine;
 
 public class Lizard : Enemy
 {
-    [SerializeField] private float _fireballSpeed, _fireballRotationSpeed, _hideDuration, _hideRadius;
+    [SerializeField] private float _fireballSpeed, _fireballRotationSpeed, _hideDuration, _hideRadius, _attackSpeed,
+        _knockbackForce, _knockbackDuration;
+    
     [SerializeField] private FireBall _fireball;
 
     private Bounds _roomBounds;
+    private GameObject _player;
+    private SpriteRenderer _spriteRenderer;
+    private CircleCollider2D _collider;
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    private Vector2 _directionToPlayer, _lizardDirection;
+
+    private bool _hidden = false, _attacking = false;
+    private bool atStart = true;
 
     void Start()
     {
-
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _collider = GetComponent<CircleCollider2D>();
+        _animator = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _lizardDirection = Vector2.right;
+        StartCoroutine(AttackLoop());
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        if(!Alive || IsBlocked) return;
+
+        _directionToPlayer = (_player.transform.position - transform.position);
+
         
+        if(_directionToPlayer.magnitude < _hideRadius && !_hidden) StartCoroutine(HideAndAppear());
+
+        _directionToPlayer = _directionToPlayer.normalized;
+        
+        if(Vector2.Dot(_directionToPlayer, _lizardDirection) < 0)
+        {
+            _spriteRenderer.flipX = !_spriteRenderer.flipX;
+
+            _lizardDirection *= -1;
+        }
+
     }
 
     public override void Attack()
     {
-        
+
+        FireBall fireball = Instantiate(_fireball, (Vector2)transform.position + new Vector2(0.5f * _lizardDirection.x, 0.5f),
+            Quaternion.AngleAxis(Mathf.Atan2(_directionToPlayer.y, _directionToPlayer.x) * Mathf.Rad2Deg, Vector3.forward));
+        fireball.Damage = _attackDamage;
+        fireball.Speed = _fireballSpeed;
+        fireball.RotationSpeed = _fireballRotationSpeed;
+        fireball.KnockbackForce = _knockbackForce;
+        fireball.KnockbackDuration = _knockbackDuration;
     }
 
-    void SelectAppearPoint()
+    IEnumerator HideAndAppear()
     {
-        Vector2 position = new Vector2(Random.Range(_roomBounds.min.x, _roomBounds.max.x), Random.Range(_roomBounds.min.y, _roomBounds.max.y));
-        transform.position = position;
+        _collider.enabled = false;
+        _animator.SetTrigger("Hide");
+        _hidden = true;
+
+        yield return new WaitForSeconds(_hideDuration);
+
+        _hidden = false;   
+        _spriteRenderer.enabled = true;
+        _animator.SetTrigger("Appear");
+        AppearAtRandomPoint();
+        _collider.enabled = true;
+        StartCoroutine(AttackLoop());
+    }
+
+    IEnumerator AttackLoop()
+    {
+        _animator.SetBool("Shoot", false);
+        if(atStart)
+        {
+            atStart = false;
+            yield return new WaitForSeconds(Random.Range(0f, 3f));
+        }
+        
+        yield return new WaitForSeconds(1f / _attackSpeed);
+        
+        if(!_hidden)
+        {
+            _animator.SetBool("Shoot", true);
+            yield return null;
+            StartCoroutine(AttackLoop());
+        }
+    }
+
+    void AppearAtRandomPoint()
+    {
+        Vector2 newPos = new Vector2(Random.Range(_roomBounds.min.x, _roomBounds.max.x), Random.Range(_roomBounds.min.y, _roomBounds.max.y));
+ 
+        transform.position = newPos;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
